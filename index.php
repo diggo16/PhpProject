1e31380d11cd9c331a5a6a237a77f062fd7906ac
dl222es <dl222es@student.lnu.se>
2015-10-15 14:10:20 +0200

    Added two model objects

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
+/*
+ * Require objects
+ */
+require_once 'model/Items.php';
 require_once 'controller/StartController.php';
 require_once 'view/layoutView.php';
-
+/*
+ * Create objects
+ */
+$items = new Items();
 $controller = new StartController();
-
 $layoutView = new LayoutView();
+
 
 $layoutView->render();
---
+++ model/Items.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Description of Items
+ *
+ * @author Daniel
+ */
+class Items 
+{
+    private $items;
+    public function __construct($items = array()) 
+    {
+        $this->items = $items;
+    }
+}
