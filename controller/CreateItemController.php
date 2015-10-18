df068c62ea1f3af6b7ab056e08af4e5c09b62dd5
dl222es <dl222es@student.lnu.se>
2015-10-19 00:52:45 +0200

    Added comments to CreateItemController

--- controller/CreateItemController.php
+++ controller/CreateItemController.php
@@ -1,22 +1,53 @@
 <?php
 /**
- * Description of CreateItemController
+ * Controller for creating a new item
  *
  * @author Daniel
  */
 class CreateItemController 
 {
+    /**
+     *
+     * @var Item $newItem
+     */
     private $newItem;
+    /**
+     *
+     * @var ErrorMessages $errorMessages
+     */
     private $errorMessages;
+    /**
+     *
+     * @var CreateItemView $view
+     */
     private $view;
+    /**
+     *
+     * @var ItemDAL $database
+     */
     private $database;
+    /**
+     *
+     * @var CreateItemRules $createItemRules
+     */
     private $createItemRules;
+    /**
+     *
+     * @var RandomString $randomString
+     */
     private $randomString;
+    /**
+     *
+     * @var Session $session
+     */
     private $session;
-    
+    /**
+     *
+     * @var boolean $isAddButtonPushed
+     */
     private $isAddButtonPushed;
     /**
-     * 
+     * Initialize objects
      * @param Item $newItem
      * @param CreateItemView $view
      */
@@ -36,12 +67,15 @@ class CreateItemController
     }
     /**
      * Check if the item is valid
+     * @param Item $newItem
+     * @param Items $items
      */
     public function CheckNewItem(Item &$newItem, Items $items)
     {
+        // If the add button is pushed
         if($this->isAddButtonPushed)
         {
-             $this->session->removeSession($this->session->getCreateItemMessage());
+            $this->session->removeSession($this->session->getCreateItemMessage());
             $title = $this->view->getTitle();
             $author = $this->view->getAuthor();
             $text = $this->view->getText();
@@ -66,10 +100,16 @@ class CreateItemController
                 $this->session->setSession($this->session->getCreateItemMessage(), $this->errorMessages->getMessageByNumber($errorNumber));
             }
         }
-       
     }
-    private function setUniqueID($items, $title, &$newItem)
+    /**
+     * Set the unique id to a generated string of the $title
+     * @param Items $items
+     * @param string $title
+     * @param Item $newItem
+     */
+    private function setUniqueID(Item $items, $title, Item &$newItem)
     {
+        // Generate a string until it is an unique
         do
         {
             $uniqueID = $this->randomString->generateUniqueID($title);
